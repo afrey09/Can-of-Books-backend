1faa3d00766719c9ea396492db1ca3483dc5d28b
Brandon Perard <Brandon.Perard@Gmail.com>
2023-01-23 12:39:01 -0800

    Added POST & DELETE endpoints

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const app = express();
 app.use(cors());
+app.use(express.json());
 const Book = require('./Models/book.js');
 mongoose.connect(process.env.DB_URL);
 
@@ -38,3 +39,35 @@ async function getBooks(request, response, next) {
     next(error);
   }
 }
+
+app.post('/books', createBook);
+
+async function createBook(request, response, next) {
+  try {
+    let createdBook = await Book.create(request.body);
+
+    response.status(200).send(createdBook);
+
+  } catch (error) {
+    console.log(error.message);
+    next(error);
+  }
+}
+
+app.delete('/books/:bookId', deleteBook);
+
+async function deleteBook(request, response, next) {
+  try {
+    let id = request.params.bookId;
+
+    await Book.findByIdAndDelete(id);
+
+    response.status(200).send('Book deleted');
+
+  } catch (error) {
+    console.log(error.message);
+    next(error);
+  }
+}
+
+
