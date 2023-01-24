a58796d6b84d846af7fa1e3a2f9266f95c40f97a
Adrienne Frey <AdrienneN.Frey@gmail.com>
2023-01-23 17:21:56 -0800

    created routes

--- seed.js
+++ seed.js
@@ -1,5 +1,6 @@
 ' Use Strict';
 
+require('dotenv').config();
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DB_URL);
 
--- server.js
+++ server.js
@@ -3,15 +3,15 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const mongoose = require ('mongoose');
+const mongoose = require('mongoose');
 const app = express();
 app.use(cors());
-
+const Book = require('./Models/book.js');
 mongoose.connect(process.env.DB_URL);
 
 const db = mongoose.connection;
-db.on('error',console.error.bind(console, 'connection error:'));
-db.once('open', function() {
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
   console.log('Mongoose is connected');
 });
 
@@ -19,8 +19,22 @@ const PORT = process.env.PORT || 3001;
 
 app.get('/test', (request, response) => {
 
-  response.send('test request received')
+  response.send('test request received');
 
-})
+});
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
+
+app.get('/books', getBooks);
+
+async function getBooks(request, response, next) {
+  try {
+    let allBooks = await Book.find({});
+
+    response.status(200).send(allBooks);
+
+  } catch (error) {
+    console.log(error.message);
+    next(error);
+  }
+}
