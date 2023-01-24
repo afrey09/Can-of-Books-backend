1a4d3a0f51e5aadcefd162d7a4d2623f03475f8d
Adrienne Frey <AdrienneN.Frey@gmail.com>
2023-01-23 16:40:38 -0800

    created schema and book objects

---
+++ seed.js
@@ -0,0 +1,35 @@
+' Use Strict';
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DB_URL);
+
+const Book = require('./Models/book.js');
+async function seed() {
+
+  await Book.create({
+    title: 'Code 101',
+    description: 'hello world!',
+    status: 'checked out',
+  });
+  console.log(`Code 101`);
+
+  await Book.create({
+    title: 'Code 102',
+    description: 'hello class',
+    status: 'checked in',
+  });
+  console.log(`Code 102`);
+
+  await Book.create({
+    title: 'Code 201',
+    description: 'good luck',
+    status: 'in progress',
+  });
+  console.log(`Code 201`);
+
+
+
+  mongoose.disconnect();
+}
+
+seed();
--- server.js
+++ server.js
@@ -3,9 +3,17 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-
+const mongoose = require ('mongoose');
 const app = express();
 app.use(cors());
+
+mongoose.connect(process.env.DB_URL);
+
+const db = mongoose.connection;
+db.on('error',console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Mongoose is connected');
+});
 
 const PORT = process.env.PORT || 3001;
 
