920ce251e5412d3dd3589d5e91a838982cd76388
Adrienne Frey <AdrienneN.Frey@gmail.com>
2023-01-25 14:30:36 -0800

    added notes

--- Models/book.js
+++ Models/book.js
@@ -3,12 +3,16 @@
 const mongoose = require ('mongoose');
 const {Schema} = mongoose;
 
+// schema is the structure of the document
+
 const bookSchema = new Schema ({
   title: { type: String, required: true},
   description: { type: String, required: true},
   status: { type: String, required: true},
 });
 
+// book is the name of the collection
 const BookModel = mongoose.model('book', bookSchema);
+
 
 module.exports = BookModel;
